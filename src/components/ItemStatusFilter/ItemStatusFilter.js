441a2b485353035052166c103155723614b6d94d
yura <yurayurev1995@gmail.com>
2022-08-07 23:15:55 +0700

    fix: ItemStatusFilter.js -> classComponent

--- src/components/ItemStatusFilter/ItemStatusFilter.js
+++ src/components/ItemStatusFilter/ItemStatusFilter.js
@@ -1,14 +1,16 @@
+import { Component } from 'react';
 import './ItemStatusFilter.css';
-
-export default function ItemStatusFilter() {
-    return (
-        <div className="btn-group">
-            <button type="button"
-                className="btn btn-success">Все</button>
-            <button type="button"
-                className="btn btn-outline-success">Активные</button>
-            <button type="button"
-                className="btn btn-outline-success">Выполненные</button>
-        </div>
-    );
-};
+export default class ItemStatusFilter extends Component {
+    render() {
+        return (
+            <div className="btn-group">
+                <button type="button"
+                    className="btn btn-success">Все</button>
+                <button type="button"
+                    className="btn btn-outline-success">Активные</button>
+                <button type="button"
+                    className="btn btn-outline-success">Выполненные</button>
+            </div>
+        );
+    }
+}
